28352b90901aa61d78fe1cd06b1e8c64e5c53429
Zachary George <zacharytgeorge@gmail.com>
2022-06-20 03:57:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,48 +3,42 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
-global randomServer
-global randUser
-global greedyDist
-global optDist
 def random_instance(serverLength, userLength):
-    randServer = np.random.uniform(low=0, high=1, size=(serverLength,))
-    randUser = np.random.uniform(low=0, high=1, size=(userLength,))
+    randUsers = np.random.uniform(low=0, high=1, size=(userLength,))
+    randServers = np.random.uniform(low=0, high=1, size=(serverLength,))
     # Use a breakpoint in the code line below to debug your script.
-def greedy():
+    return randUsers, randServers
+def greedy(randUsers, randServers):
     greedyDist = 0.0
-    totalDist = [9999] * len(randUser)
-    for i in range(len(randUser)): #Shorter of the distances
-        for j in range(len(randUser)):
-            if(randomServer[i]-randUser[j]<totalDist[i] or ((randomServer[i] - randUser[j]) * -1 < totalDist[i]) ):
-                if(randomServer[i] - randUser[j] < 0):
-                    randUser.remove(randUser[j])
-                    totalDist[i] = (randomServer[i] - randUser[j]) * -1
-                else:
-                    randUser.remove(randUser[j])
-                    totalDist[i] = (randomServer[i] - randUser[j])
+    totalDist = [9999] * len(randUsers)
+    removeVar = 0
+    for i in range(len(randUsers)):
+        for j in range(len(randServers)):
+            if(abs(randUsers[i]-randServers[j])<totalDist[i]):
+                    totalDist[i] = abs(randUsers[i] - randServers[j])
+                    removeVar = j #Using removevar to be the last one until i increments
+        randServers.remove(randServers[removeVar])
     for i in range(len(totalDist)):
         greedyDist += totalDist[i]
+    return greedyDist
     #Matching using the greedy algorithm based on array incremental points
     #match the algo
-def opt ():
+def opt (randUsers, randServers):
     optDist = 0.0
     minUser = 0.0
     minServer = 0.0
     i = 0
-    while(i in range(len(randUser))):
-        minUser = min(randUser)
-        minServer = min(randomServer)
-        if(minServer - minUser < 0):
-            optDist += (minServer - minUser)*-1
-        else:
-            optDist += (minServer - minUser)
-        randUser.remove(min(randUser))
-        randomServer.remove(min(randomServer))
-        i += 1
+    while(len(randUsers) > 0):
+        minUser = min(randUsers)
+        minServer = min(randServers)
+        if(abs(minServer - minUser) < 0):
+            optDist += abs(minServer - minUser)
+        randUsers.remove(min(randUsers))
+        randServers.remove(min(randServers))
+    return optDist
     #Based on what we know matching looking throughout the entire array
-def evaluate():
-    return greedyDist - optDist
+def evaluate(greedyDist, optDist):
+    return greedyDist/optDist
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Debugging')
