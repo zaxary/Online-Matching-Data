c7cccc91cb95b0fdc3f1f96c7da55bf3b41b07a1
Zachary George <zacharytgeorge@gmail.com>
2022-06-24 10:02:08 -0400

    Implemented main method

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def greedy(randUsers, randServers):
             if(abs(randUsers[i]-randServers[j])<totalDist[i]):
                     totalDist[i] = abs(randUsers[i] - randServers[j])
                     removeVar = j #Using removevar to be the last one until i increments
-        randServers.remove(randServers[removeVar])
+        randServers.tolist().remove(randServers[removeVar])
     for i in range(len(totalDist)):
         greedyDist += totalDist[i]
     return greedyDist
@@ -25,22 +25,39 @@ def greedy(randUsers, randServers):
     #match the algo
 def opt (randUsers, randServers):
     optDist = 0.0
-    minUser = 0.0
-    minServer = 0.0
     i = 0
-    while(len(randUsers) > 0):
+    while(i in range(len(randServers))):
         minUser = min(randUsers)
         minServer = min(randServers)
-        if(abs(minServer - minUser) < 0):
-            optDist += abs(minServer - minUser)
-        randUsers.remove(min(randUsers))
-        randServers.remove(min(randServers))
+        optDist += abs(minServer - minUser)
+        randUsers.tolist().remove(min(randUsers))
+        randServers.tolist().remove(min(randServers))
+        i += 1
     return optDist
     #Based on what we know matching looking throughout the entire array
 def evaluate(greedyDist, optDist):
     return greedyDist/optDist
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('Debugging')
-
+    result = random_instance(10, 10)
+    print('Debugging random_instance')
+    users = result[0]
+    print('Debugging return [0]')
+    servers = result[1]
+    print('Debugging return [1]')
+    greedy_dist = greedy(users, servers)
+    print('Debugging greedy_dist ' + str(greedy_dist))
+    opt_dist = opt(users, servers)
+    print('Debugging opt_dist ' + str(opt_dist))
+    print(evaluate(greedy_dist, opt_dist))
+    totalSum = 0
+    trials = 100
+    for i in range(trials):
+        result = random_instance(10, 10)
+        users = result[0]
+        servers = result[1]
+        greedy_dist = greedy(users, servers)
+        opt_dist = opt(users, servers)
+        totalSum += evaluate(greedy_dist, opt_dist)
+    print(totalSum/trials)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
